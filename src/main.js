f29fa62648ca66047799f4ab6dd9f086b428255e
Slomer <sam.padronhernandez@ucalgary.ca>
2025-01-18 13:55:33 -0700

    Update

--- src/main.js
+++ src/main.js
@@ -43,9 +43,9 @@ document.querySelector("#app").innerHTML = `
       <h2>Sources</h2>
       <div id="project-row">
         <div id="images-description">
-          <h3>A 3D house model is a geometric representation of a house, featuring walls, a roof, and windows. 
+          <h4 id="description">A 3D house model is a geometric representation of a house, featuring walls, a roof, and windows. 
           It often includes additional details such as doors, chimneys, and balconies, with textures like brick, 
-          wood, or stucco to enhance realism. The interior may also feature rooms, furniture, and lighting for a more detailed design.</h3>
+          wood, or stucco to enhance realism. The interior may also feature rooms, furniture, and lighting for a more detailed design.</h4>
           <div id="images">
             <a href="https://peaksandstreams.com/2020/08/26/andy-good-peak-23-august-2020/" target="_blank">
               <img src="./sourceImages/andygood.jpg" alt="Andygood" />
